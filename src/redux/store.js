7c5b97ec73aff40ce28e90cd6d5aa129fdfcf1d1
Irfnekf <vladshkaraputa07@gmail.com>
2023-02-27 17:34:33 +0200

    error

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,10 @@ import rootReducer from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
